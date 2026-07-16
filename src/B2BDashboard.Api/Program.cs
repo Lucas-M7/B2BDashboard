ceb7ba5216370de02912a06cab724ee316a78a6d
Lucas-M7 <Lucas015x@gmail.com>
2026-07-16 20:07:59 -0300

    feat: implementa a dependencia do swagger e configura no Program.cs junto com o tratamento global de excecoes

--- src/B2BDashboard.Api/Program.cs
+++ src/B2BDashboard.Api/Program.cs
@@ -1,3 +1,4 @@
+using B2BDashboard.Api.ErrorHandling;
 using B2BDashboard.Application.Interfaces;
 using B2BDashboard.Application.Services;
 using B2BDashboard.Infrastructure.Persistence;
@@ -9,8 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+#region Banco de dados
 builder.Services.AddDbContext<AppDbcontext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnections")));
+#endregion
 
 #region Repositórios
 builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
@@ -25,11 +28,23 @@ builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddScoped<ISaleService, SaleService>();
 #endregion
 
+#region Tratamento global de exceções
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+#endregion
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();

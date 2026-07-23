ff91dac8bb0234ab5ea06e1a3dbd5679cf551f18
Lucas-M7 <Lucas015x@gmail.com>
2026-07-23 20:40:20 -0300

    feat: implement the POCO class

---
+++ src/B2BDashboard.Application/Common/JwtSettings.cs
@@ -0,0 +1,10 @@
+namespace B2BDashboard.Application.Common;
+
+public class JwtSettings
+{
+    public string Key { get; init; } = string.Empty;
+    public string Issuer { get; init; } = string.Empty;
+    public string Audience { get; init; } = string.Empty;
+    public int AccessTokenExpirationMinutes { get; init; } = 15;
+    public int RefreshTokenExpirationDays { get; init; } = 7;
+}
